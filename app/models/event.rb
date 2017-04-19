7a2eb1df19a7723fe405ac6c05d2ffc63cee6464
Antarts <m.dream@foxmail.com>
2017-04-19 20:51:27 +0800

    add status and users list

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,20 +1,25 @@
 class Event < ApplicationRecord
 
- validates_presence_of :name, :friendly_id
+  STATUS = ["draft", "public", "private"]
+  validates_inclusion_of :status, :in => STATUS
 
- validates_uniqueness_of :friendly_id
- validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+  validates_presence_of :name, :friendly_id
 
- before_validation :generate_friendly_id, :on => :create
+  validates_uniqueness_of :friendly_id
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
 
- def to_param
-   self.friendly_id
- end
+  before_validation :generate_friendly_id, :on => :create
 
- protected
+  belongs_to :category, :optional => true
 
- def generate_friendly_id
-   self.friendly_id ||= SecureRandom.uuid
- end
+  def to_param
+    self.friendly_id
+  end
+
+  protected
+
+  def generate_friendly_id
+    self.friendly_id ||= SecureRandom.uuid
+  end
 
 end
