fab7de3cf8cac610247ca3b5c0f6b23a277aafdc
Antarts <m.dream@foxmail.com>
2017-05-03 15:30:47 +0800

    add ticket model & datepicker & admin css js $ rich editor & bulk editing

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require select2
+//= require nested_form_fields
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.js
+//= require ckeditor/init
+
