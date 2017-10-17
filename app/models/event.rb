ecc92357000c3b328b30a3bfa010afefe89de055
Antarts <m.dream@foxmail.com>
2017-10-17 11:16:19 +0800

    add status to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
 
  validates_presence_of :name, :friendly_id
 
+ STATUS = ["draft", "public", "private"]
+ validates_inclusion_of :status, :in => STATUS
+
  validates_uniqueness_of :friendly_id
  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]\z/
 
